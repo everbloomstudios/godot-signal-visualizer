0de5228572f74d9a890afc0a7fcb6675a05860bf
Energyxxer <energyxxer@gmail.com>
2025-02-13 11:11:48 -0400

    Add AuthorityMode, add extra logic icons

---
+++ LogicalNodes/AuthorityMode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Godot;
+
+namespace LogicalNodes;
+
+public enum AuthorityMode {
+    Authority,
+    AnyPeer,
+    NonAuthority
+}
+
+public static class AuthorityModeExt
+{
+    public static bool CanExecute(this AuthorityMode mode, Node node)
+    {
+        return mode switch
+        {
+            AuthorityMode.Authority => node.IsMultiplayerAuthority(),
+            AuthorityMode.AnyPeer => true,
+            AuthorityMode.NonAuthority => !node.IsMultiplayerAuthority(),
+            _ => throw new ArgumentOutOfRangeException(nameof(mode), mode, null)
+        };
+    }
+}
