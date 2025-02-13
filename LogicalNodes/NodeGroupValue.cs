c24fe4691024e7d915c243b715670054b1224102
Energyxxer <energyxxer@gmail.com>
2025-02-13 17:11:59 -0400

    Add node group value source

---
+++ LogicalNodes/NodeGroupValue.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Godot;
+
+namespace LogicalNodes;
+
+[Tool]
+[GlobalClass]
+[Icon($"{LogicalNodeIcons.IconRoot}/node_group.png")]
+public partial class NodeGroupValue : ValueSource, IEnumerableValueSource<Node>, IEnumerableValueSource<Variant>
+{
+    [Export] public StringName GroupName;
+    
+    public override Variant GetValue(Node source)
+    {
+        return source.GetTree().GetFirstNodeInGroup(GroupName);
+    }
+
+    IEnumerable<Variant> IEnumerableValueSource<Variant>.EnumerateValues(Node source)
+    {
+        foreach (var node in source.GetTree().GetNodesInGroup(GroupName))
+        {
+            yield return node;
+        }
+    }
+
+    IEnumerable<Node> IEnumerableValueSource<Node>.EnumerateValues(Node source)
+    {
+        foreach (var node in source.GetTree().GetNodesInGroup(GroupName))
+        {
+            yield return node;
+        }
+    }
+}
