ba83e0c92fec5198bfad68614bfc99561c64bbb9
Energyxxer <energyxxer@gmail.com>
2025-03-11 12:15:06 -0400

    Add overload for AuthorityMode CanExecute for outside tree

--- LogicalNodes/AuthorityMode.cs
+++ LogicalNodes/AuthorityMode.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CloneSwap.Systems.Multiplayer;
 using Godot;
 
 namespace LogicalNodes;
@@ -18,6 +19,16 @@ public static class AuthorityModeExt
             AuthorityMode.Authority => node.IsMultiplayerAuthority(),
             AuthorityMode.AnyPeer => true,
             AuthorityMode.NonAuthority => !node.IsMultiplayerAuthority(),
+            _ => throw new ArgumentOutOfRangeException(nameof(mode), mode, null)
+        };
+    }
+    public static bool CanExecuteNoTree(this AuthorityMode mode, Node node)
+    {
+        return mode switch
+        {
+            AuthorityMode.Authority => node.GetMultiplayerAuthority() == MultiplayerManager.Instance.Multiplayer.GetUniqueId(),
+            AuthorityMode.AnyPeer => true,
+            AuthorityMode.NonAuthority => node.GetMultiplayerAuthority() != MultiplayerManager.Instance.Multiplayer.GetUniqueId(),
             _ => throw new ArgumentOutOfRangeException(nameof(mode), mode, null)
         };
     }
