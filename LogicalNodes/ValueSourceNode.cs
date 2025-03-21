7ac00dd7640b355b26e0890f3607cae4e08af688
Energyxxer <energyxxer@gmail.com>
2025-03-21 14:42:56 -0400

    Add parameterless GetValue overloads for ValueSourceNode

--- LogicalNodes/ValueSourceNode.cs
+++ LogicalNodes/ValueSourceNode.cs
@@ -10,6 +10,9 @@ public partial class ValueSourceNode : Node, IValueSource
     [Export]
     public ValueSource ValueSource;
 
+    public Variant GetValue() => GetValue(null);
+    public T GetValue<[MustBeVariant]T>() => GetValue<T>(null);
+
     public Variant GetValue(Node source)
     {
         return ValueSource?.GetValue(this) ?? default;
